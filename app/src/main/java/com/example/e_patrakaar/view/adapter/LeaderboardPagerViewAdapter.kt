6bd905c5a932c5daa7fec90a95a1093990434c5b
Vibhu Vineet Kumar <vibhuvineet52@gmail.com>
2022-10-14 01:17:38 +0530

    Leaderboard part initial commit

---
+++ app/src/main/java/com/example/e_patrakaar/view/adapter/LeaderboardPagerViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.e_patrakaar.view.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.e_patrakaar.view.fragment.HealthFragment
+import com.example.e_patrakaar.view.fragment.SportsFragment
+
+class LeaderboardPagerViewAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> {
+                SportsFragment()
+            }
+            1 -> {
+                HealthFragment()
+            }
+            else -> {
+                Fragment()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/e_patrakaar/view/fragment/LeaderboardFragment.kt
+++ app/src/main/java/com/example/e_patrakaar/view/fragment/LeaderboardFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.e_patrakaar.databinding.FragmentLeaderboardBinding
+import com.example.e_patrakaar.view.adapter.LeaderboardPagerViewAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class LeaderboardFragment : Fragment() {
 
@@ -18,6 +20,25 @@ class LeaderboardFragment : Fragment() {
     ): View {
 
         binding = FragmentLeaderboardBinding.inflate(inflater, container, false)
+
+        val tabLayout = binding?.tabLayout
+        val viewPager2 = binding?.viewPager2
+
+        val adapter =
+            LeaderboardPagerViewAdapter(requireActivity().supportFragmentManager, lifecycle)
+
+        viewPager2?.adapter = adapter
+
+        TabLayoutMediator(tabLayout!!, viewPager2!!) { tab, position ->
+            when (position) {
+                0 -> {
+                    tab.text = "Sports"
+                }
+                1 -> {
+                    tab.text = "Health"
+                }
+            }
+        }.attach()
 
         return binding!!.root
     }
