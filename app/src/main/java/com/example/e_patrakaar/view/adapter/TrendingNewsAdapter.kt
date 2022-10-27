1712a8c2f37092aa282926fa673b221e63733821
IamBansal <abansal11ae@gmail.com>
2022-10-27 12:23:28 +0530

    Expanded News

--- app/src/main/java/com/example/e_patrakaar/view/adapter/TrendingNewsAdapter.kt
+++ app/src/main/java/com/example/e_patrakaar/view/adapter/TrendingNewsAdapter.kt
@@ -3,15 +3,21 @@ package com.example.e_patrakaar.view.adapter
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.e_patrakaar.R
 import com.example.e_patrakaar.databinding.CustomTrendingItemBinding
 import com.example.e_patrakaar.model.Collection
+import com.example.e_patrakaar.view.fragment.leaderboard.SportsFragment
+import com.example.e_patrakaar.view.fragment.main.HomeFragment
 
 class TrendingNewsAdapter(private val fragment: Fragment, private val list: List<Collection>): RecyclerView.Adapter<TrendingNewsAdapter.ViewHolder>() {
 
     class ViewHolder(view: CustomTrendingItemBinding) : RecyclerView.ViewHolder(view.root) {
         val text: TextView = view.textView
+        val card: CardView = view.card
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -19,8 +25,18 @@ class TrendingNewsAdapter(private val fragment: Fragment, private val list: List
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val dish = list[position]
-        holder.text.text = dish.text1
+        val news = list[position]
+        holder.text.text = news.text1
+        holder.card.setOnClickListener {
+            when(fragment){
+              is HomeFragment -> {
+                  fragment.findNavController().navigate(R.id.action_navigation_home_to_navigation_expanded_news)
+              }
+              is SportsFragment -> {
+                  fragment.findNavController().navigate(R.id.action_navigation_leaderboard_to_navigation_expanded_news)
+              }
+            }
+        }
     }
 
     override fun getItemCount(): Int {
